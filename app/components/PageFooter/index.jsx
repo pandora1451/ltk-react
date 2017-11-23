de2a204b9629be6aafc68737a966731903a4cbe3
Jason <vvcc1117@sina.com>
2017-11-23 19:54:27 +0800

    1954

--- app/components/PageFooter/index.jsx
+++ app/components/PageFooter/index.jsx
@@ -11,21 +11,21 @@ class PageFooter extends React.Component {
     render() {
         return (
             <section className="pagefooter">
-            	<div>
+            	<div className="tabContainer active">
             		<img src={require('../../static/images/tab/12x.png')}/>
             		<div><span>首页</span></div>
             	</div>
-            	<div>
-            		<img src={require('../../static/images/tab/12x.png')}/>
-            		<div><span>首页</span></div>
+            	<div className="tabContainer">
+            		<img src={require('../../static/images/tab/22@2x.png')}/>
+            		<div><span>分类</span></div>
             	</div>
-            	<div>
-            		<img src={require('../../static/images/tab/12x.png')}/>
-            		<div><span>首页</span></div>
+            	<div className="tabContainer">
+            		<img src={require('../../static/images/tab/33@2x.png')}/>
+            		<div><span>购物车</span></div>
             	</div>
-            	<div>
-            		<img src={require('../../static/images/tab/12x.png')}/>
-            		<div><span>首页</span></div>
+            	<div className="tabContainer">
+            		<img src={require('../../static/images/tab/44@2x.png')}/>
+            		<div><span>我的</span></div>
             	</div>
             </section>
         )
