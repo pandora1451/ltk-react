7aa638147667a6e7e6694f32086eaa00726edaaf
Jason <vvcc1117@sina.com>
2017-11-23 15:07:19 +0800

    添加文件

---
+++ mock/home/ad.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        title: '暑假5折',
+        img: 'http://www.lipstick2017.com/images/201707/goods_img/731_G_1499305791990.jpg',
+        link: 'http://www.imooc.com/wap/index'
+    },
+    {
+        title: '特价出国',
+        img: 'http://www.lipstick2017.com/images/201707/goods_img/731_G_1499305791990.jpg',
+        link: 'http://www.imooc.com/wap/index'
+    },
+    {
+        title: '亮亮车',
+        img: 'http://www.lipstick2017.com/images/201707/goods_img/731_G_1499305791990.jpg',
+        link: 'http://www.imooc.com/wap/index'
+    },
+    {
+        title: '学钢琴',
+        img: 'http://www.lipstick2017.com/images/201707/goods_img/731_G_1499305791990.jpg',
+        link: 'http://www.imooc.com/wap/index'
+    },
+    {
+        title: '电影',
+        img: 'http://www.lipstick2017.com/images/201707/goods_img/731_G_1499305791990.jpg',
+        link: 'http://www.imooc.com/wap/index'
+    },
+    {
+        title: '旅游热线',
+        img: 'http://www.lipstick2017.com/images/201707/goods_img/731_G_1499305791990.jpg',
+        link: 'http://www.imooc.com/wap/index'
+    }
+]
---
+++ mock/orderlist/orderList.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id: Date.now(),
+        img: 'http://www.lipstick2017.com/images/201707/goods_img/731_G_1499305791990.jpg',
+        title: '汉堡大王',
+        count: 3,
+        price: '167',
+        commentState: 0
+    },
+    {
+        id: Date.now(),
+        img: 'http://www.lipstick2017.com/images/201707/goods_img/731_G_1499305791990.jpg',
+        title: '麻辣香锅',
+        count: 1,
+        price: '188',
+        commentState: 0
+    },
+    {
+        id: Date.now(),
+        img: 'http://www.lipstick2017.com/images/201707/goods_img/731_G_1499305791990.jpg',
+        title: '好吃自出餐',
+        count: 2,
+        price: '110',
+        commentState: 2
+    }
+]
