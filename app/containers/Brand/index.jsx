25f4ef0acda84bcfefdef7ded34eb0639fb79fca
Jason <vvcc1117@sina.com>
2017-12-15 11:46:11 +0800

    品牌页面

--- app/containers/Brand/index.jsx
+++ app/containers/Brand/index.jsx
@@ -26,6 +26,62 @@ class Brand extends React.Component {
         					我们从经济现象中获得灵感，创造了口红效应平台，致力于为顾客提供极具性价比，而又在非凡意义的商品。你会在平台上获得除了产品本身之外，更有意义的购物体验。口红效应的初衷是让每一位顾客都变得深入骨髓的精致。
         				</span>
         			</div>
+        		</div>
+        		<div className="goods-list">
+        			<div className="title">
+        				<div className="right">
+        					<span className="sign active">最新</span><span className="shuxian">|</span><span className="sign">最热</span>
+        				</div>
+        				<div className="left">
+        					<i className="iocn-brand"></i><span>23</span><span>款商品</span>
+        				</div>
+        			</div>
+        			<div className="list">
+        				<div className="item">
+        					<div className="img-box">
+        						<img src={require('../../static/images/goods/357_G_1493767698760.png')}/>
+        					</div>
+        					<div className="goods-name">
+        						<span>Pola White Shot美白修护养颜口服液</span>
+        					</div>
+        					<div className="price">
+        						<span className="price-now">¥ 3290.00</span><del className="price-market">¥ 3490.00</del>
+        					</div>
+        				</div>
+        				<div className="item">
+        					<div className="img-box">
+        						<img src={require('../../static/images/goods/357_G_1493767698760.png')}/>
+        					</div>
+        					<div className="goods-name">
+        						<span>Pola White Shot美白修护养颜口服液</span>
+        					</div>
+        					<div className="price">
+        						<span className="price-now">¥ 3290.00</span><del className="price-market">¥ 3490.00</del>
+        					</div>
+        				</div>
+        				<div className="item">
+        					<div className="img-box">
+        						<img src={require('../../static/images/goods/357_G_1493767698760.png')}/>
+        					</div>
+        					<div className="goods-name">
+        						<span>Pola White Shot美白修护养颜口服液</span>
+        					</div>
+        					<div className="price">
+        						<span className="price-now">¥ 3290.00</span><del className="price-market">¥ 3490.00</del>
+        					</div>
+        				</div>
+        				<div className="item">
+        					<div className="img-box">
+        						<img src={require('../../static/images/goods/357_G_1493767698760.png')}/>
+        					</div>
+        					<div className="goods-name">
+        						<span>Pola White Shot美白修护养颜口服液</span>
+        					</div>
+        					<div className="price">
+        						<span className="price-now">¥ 3290.00</span><del className="price-market">¥ 3490.00</del>
+        					</div>
+        				</div>
+        			</div>
         		</div>				        		
         	</section>
         )
